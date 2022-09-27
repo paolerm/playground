e3f31b3c49bb469348ceb0c3fa5d7930c29798de
paolo ermini <paermini@microsoft.com>
2022-09-27 15:45:10 -0700

    test CSI driver

--- hello-world/src/app.ts
+++ hello-world/src/app.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
+import * as fs from 'fs';
 
 const app = express();
 const version = '1.0';
 
-app.get('/', (req, res) => res.send(`Hello World! This is the ${version} version`));
+const secret = fs.readFileSync('/mnt/secrets-store/csi-please-open-me');
+
+app.get('/', (req, res) => res.send(`Hello World! This is the ${version} version. The secret value is: ${secret}`));
 app.listen(3000, () => console.log('Server ready'));
 
