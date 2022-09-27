fe5bb46a57591c3683d01dd6efba1c4c6ba66ca5
paolo ermini <paermini@microsoft.com>
2022-09-27 10:15:39 -0700

    added yml files for load balancer and deploy

--- hello-world/src/app.ts
+++ hello-world/src/app.ts
@@ -1,7 +1,8 @@
 import * as express from 'express';
 
 const app = express();
+const version = '1.0';
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(`Hello World! This is the ${version} version`));
 app.listen(3000, () => console.log('Server ready'));
 
